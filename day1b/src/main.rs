8b454579d3573f173fa1b8828acef94d0cd12eca
tajo48 <tajo48@proton.me>
2023-12-01 20:11:53 +0100

    1b

--- day1b/src/main.rs
+++ day1b/src/main.rs
@@ -1,9 +1,29 @@
 pub fn main() {
     let kek = include_str!("../input.txt");
-    println!("{}", sum_and_delete(kek));
+    println!("{}", sum_and_delete(letter_to_digit(kek.to_string())));
 }
 
-fn sum_and_delete(kek: &str) -> u32{
+fn letter_to_digit(input: String) -> String{
+    let numbers = vec!["----", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    let mut output:String = "".to_string();
+    let mut creating_line:String="".to_string();
+    // function to make eightwothree to 83 and not 823
+    for line in input.lines() {
+        for chars in line.chars() {
+            creating_line = creating_line + &chars.to_string();
+            for (index, number) in numbers.iter().enumerate() {
+            creating_line = creating_line.replace(number, &index.to_string());
+            }
+        }
+    output = output + &creating_line + "\n";
+    creating_line = "".to_string(); 
+    println!("{}", output);
+    }
+
+    return output 
+}
+
+fn sum_and_delete(kek: String) -> u32{
     let mut sum = 0;
     for line in kek.lines() {
         let num:u32 = (line.replace(|c: char| c.is_alphabetic(), "")).parse().unwrap();
